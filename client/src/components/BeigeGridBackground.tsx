32cebc873b3abed79a74d807f0be27459dd6cf49
Brian <brianzninja@gmail.com>
2026-09-13 19:11:58 -0400

    polish

--- client/src/components/BeigeGridBackground.tsx
+++ client/src/components/BeigeGridBackground.tsx
@@ -1,16 +1,3 @@
-import GridDistortion from './GridDistortion.tsx'
-
 export function BeigeGridBackground() {
-  return (
-    <div className="grid-bg" aria-hidden>
-      <GridDistortion
-        imageSrc="https://images.unsplash.com/photo-1557682250-33bd709cbe85?w=800"
-        grid={15}
-        mouse={0.1}
-        strength={0.15}
-        relaxation={0.9}
-      />
-      <div className="grid-bg-tint" />
-    </div>
-  )
+  return <div className="grid-bg" aria-hidden />
 }
